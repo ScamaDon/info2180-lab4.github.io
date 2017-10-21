ab97557d7cf8435bc467c88adcc48409fabd9708
ScamaDon <barrettaldane@ymail.com>
2017-10-20 21:54:08 -0400

    Add files via upload

---
+++ maze.js
@@ -0,0 +1,47 @@
+var loser = false;
+var cheat = false;
+windows.onload = function(){
+  $("start").onclick = start;
+  $("start").onmouseover = hover;
+  $("end").onmouseleave = end;
+
+  var wall = $$("#maze div.boundary");
+  for (var i = 0; i < wall.length, i++){
+    wall[i].onmouseover = endgame;
+  }
+}
+
+function endGame() {
+    lose = true;
+    cheat = false;
+    $("status").textContent = "You lose!!";
+    var wall = $$("#maze div.boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+        wall[i].addClassName("youlose");
+    }
+}
+
+function end() {
+    if (!lose && !cheat) {
+        $("status").textContent = "You win!";
+    }
+    else if(cheat && !lose){
+        $("status").textContent = "You Tried to cheat I see!";
+        var wall = $$("#maze div.boundary");
+        for (var i = 0; i < boundaries.length; i++) {
+            wall[i].addClassName("youlose");
+        }
+    }
+}
+function start() {
+      lose = false;
+      cheat = false;
+      $("status").textContent = "Find the end!";
+      var wall = $$("#maze div.boundary");
+      for (var i = 0; i < boundaries.length; i++) {
+          wall[i].removeClassName("youlose");
+      }
+  }
+function cheat(){
+  cheat = true;
+}
